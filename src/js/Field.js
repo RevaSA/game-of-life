2cbfaaa0dfc4068c90b813fa7db468971f504066
Stanislav Reva <RevaSA95@gmail.com>
2020-01-22 09:34:42 +0300

    Move cell processing to field

--- src/js/Field.js
+++ src/js/Field.js
@@ -1,20 +1,74 @@
 import settings from './settings';
+import Cell from './Cell';
 
 class Field {
     constructor(ctx) {
         this.ctx = ctx;
     }
 
-    updateSize(width, height) {
+    updateSizes(width, height) {
         this.countCol = Math.floor(width / settings.size.cell);
         this.countRow = Math.floor(height / settings.size.cell);
         this.w = this.countCol * settings.size.cell;
         this.h = this.countRow * settings.size.cell;
         this.x = Math.floor((width - this.w) / 2);
         this.y = Math.floor((height - this.h) / 2);
+        this.updateCells();
+    }
+
+    updateCells() {
+        this.cells = [];
+
+        for (let i = 0; i < this.countRow; i++) {
+            this.cells.push([]);
+
+            for (let j = 0; j < this.countCol; j++) {
+                const x = this.x + j * settings.size.cell;
+                const y = this.y + i * settings.size.cell;
+
+                this.cells[i].push(new Cell(this.ctx, x, y, i, j));
+            }
+        }
+
+        for (let i = 0; i < this.countRow; i++) {
+            for (let j = 0; j < this.countCol; j++) {
+                this.cells[i][j].setNeighbors(this.getNeighbors(i, j))
+            }
+        }
+    }
+
+    getNeighbors(rowSource, colSource) {
+        const neighbors = [];
+        let i, j;
+
+        for (i = -1; i <= 1; i++) {
+            for (j = -1; j <= 1; j++) {
+                if (i === 0 && j === 0) {
+                    continue;
+                }
+
+                let row = rowSource + i;
+                let col = colSource + j;
+
+                row = row < 0 ? this.countRow - 1 : row >= this.countRow ? 0 : row;
+                col = col < 0 ? this.countCol - 1 : col >= this.countCol ? 0 : col;
+                neighbors.push(this.cells[row][col]);
+            }
+        }
+
+        return neighbors;
+    }
+
+    drawCells() {
+        for (let i = 0; i < this.countRow; i++) {
+            for (let j = 0; j < this.countCol; j++) {
+                this.cells[i][j].draw();
+            }
+        }
     }
 
     draw() {
+        this.drawCells();
         this.drawBorders();
     }
 
--- src/js/Game.js
+++ src/js/Game.js
@@ -1,6 +1,5 @@
 import settings from './settings';
 import Field from './Field';
-import Cell from './Cell';
 
 class Game {
     constructor(options) {
@@ -25,63 +24,10 @@ class Game {
         this.h = this.canvas.clientHeight;
         this.canvas.width = this.w;
         this.canvas.height = this.h;
-        this.field.updateSize(this.w, this.h);
-        this.updateCells();
-    }
-
-    updateCells() {
-        this.cells = [];
-
-        for (let i = 0; i < this.field.countRow; i++) {
-            this.cells.push([]);
-
-            for (let j = 0; j < this.field.countCol; j++) {
-                const x = this.field.x + j * settings.size.cell;
-                const y = this.field.y + i * settings.size.cell;
-
-                this.cells[i].push(new Cell(this.ctx, x, y, i, j));
-            }
-        }
-
-        for (let i = 0; i < this.field.countRow; i++) {
-            for (let j = 0; j < this.field.countCol; j++) {
-                this.cells[i][j].setNeighbors(this.getNeighbors(i, j))
-            }
-        }
-    }
-
-    getNeighbors(rowSource, colSource) {
-        const neighbors = [];
-        let i, j;
-
-        for (i = -1; i <= 1; i++) {
-            for (j = -1; j <= 1; j++) {
-                if (i === 0 && j === 0) {
-                    continue;
-                }
-
-                let row = rowSource + i;
-                let col = colSource + j;
-
-                row = row < 0 ? this.field.countRow - 1 : row >= this.field.countRow ? 0 : row;
-                col = col < 0 ? this.field.countCol - 1 : col >= this.field.countCol ? 0 : col;
-                neighbors.push(this.cells[row][col]);
-            }
-        }
-
-        return neighbors;
-    }
-
-    drawCells() {
-        for (let i = 0; i < this.field.countRow; i++) {
-            for (let j = 0; j < this.field.countCol; j++) {
-                this.cells[i][j].draw();
-            }
-        }
+        this.field.updateSizes(this.w, this.h);
     }
 
     loop() {
-        this.drawCells();
         this.field.draw();
         requestAnimationFrame(this.loop.bind(this));
     }
